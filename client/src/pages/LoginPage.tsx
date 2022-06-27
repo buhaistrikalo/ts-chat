207002d20d571c85cd6ccd876d2c2ea28b77cb21
Kirill <kirilltcoi@yandex.ru>
2022-06-27 17:38:22 +0700

    init

---
+++ client/src/pages/LoginPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Center from 'layout/Center';
+
+const LoginPage = () => {
+    return (
+        <Center>
+            <div>LoginPage</div>
+        </Center>
+    );
+};
+
+export default LoginPage;
